5c1074f673a8c1a394b505c2c0edc47591cfcd6a
koru1130 <koru1130@gmail.com>
2016-01-09 01:06:33 +0800

    fix bug

--- owo.js
+++ owo.js
@@ -1,12 +1,13 @@
 function getUserData(){
   if($("#user_id").val()>100 || $("#user_id").val()<1){
-    alert("Invalid userid, userid should be between 1 and 100");
+    $("#errorText").html("Invalid userid, userid should be between 1 and 100");
+  }else {
+      $("#errorText").html("");
+    $.getJSON('http://jsonplaceholder.typicode.com/posts/'+$("#user_id").val(),function(data){
+      $("#cellUserId").html(data.userId);
+      $("#cellId").html(data.id);
+      $("#cellTitle").html(data.title);
+      $("#cellBody").html(data.body);
+    });
   }
-  $.getJSON('http://jsonplaceholder.typicode.com/posts/'+$("#user_id").val(),function(data){
-    $("#cellUserId").html(data.userId);
-    $("#cellId").html(data.id);
-    $("#cellTitle").html(data.title);
-    $("#cellBody").html(data.body);
-  })
 }
-s
