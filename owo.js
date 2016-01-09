13370c7937ad170ae3fd4f131dc3134dcf3ecba0
koru1130 <koru1130@gmail.com>
2016-01-09 20:57:47 +0800

    add Loading...

--- owo.js
+++ owo.js
@@ -3,7 +3,11 @@ function getUserData(){
     $("#errorText").html("Invalid userid, userid should be between 1 and 100");
   }else {
       $("#errorText").html("");
+      $("#btnQuery").html("Loading...");
+      $("#btnQuery").prop('disabled', true);
     $.getJSON('http://jsonplaceholder.typicode.com/posts/'+$("#user_id").val(),function(data){
+      $("#btnQuery").html("Query");
+      $("#btnQuery").prop('disabled', false);
       $("#cellUserId").html(data.userId);
       $("#cellId").html(data.id);
       $("#cellTitle").html(data.title);
